6467935e3170fcbfcff184c25ac1d3ec37d13764
Don Currie <d.currie@utoronto.ca>
2015-02-08 01:45:11 -0500

    add blog_spec

---
+++ blog_spec.rb
@@ -0,0 +1,15 @@
+require 'minitest/autorun'
+require "./blog"
+
+describe Blog do 
+	before do
+		@blog = Blog.new
+	end
+
+	describe '#title' do
+		it "returns the title of the blog" do
+			@blog.title.must_equal "Treehouse Blog"
+		end
+	end
+
+end
